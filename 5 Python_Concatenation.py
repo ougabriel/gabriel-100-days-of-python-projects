04e3bf56ea1c116f75b6354228b37b395aff1231
Gabriel-Okom <ougabriel@gmail.com>
2025-01-03 12:30:10 +0000

    Added Day 1 files and projects by Gabriel Okom

--- 5 Python_Concatenation.py
+++ 5 Python_Concatenation.py
@@ -1,10 +1,17 @@
-
-
-
-###Adding a space inbetween the fullname
+#This line of code addes two strings together
 name = input("What is your name?")
 surname = input("What is your surname?")
 
-fullname = name + " " + surname
+fullname = name + surname
+
+print(fullname)
+
+
+
+###Adding a space and exclamation mark inbetween the fullname
+name = input("What is your name?")
+surname = input("What is your surname?")
+
+fullname = name + " " + surname + "!"
 
 print(fullname)
