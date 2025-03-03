7c380de710611666696e93c19fdba8ad8c85f263
Gabriel-Okom <ougabriel@gmail.com>
2025-03-03 00:37:46 +0000

    Modified rock, paper, scissors game code

--- Day 4/Rock Paper Scissor Game Project/solution.py
+++ Day 4/Rock Paper Scissor Game Project/solution.py
@@ -1,64 +1,53 @@
 import random
-
-# Rock Paper Scissors ASCII Art
-# Rock
-rock = ("""
+rock = '''
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-""")
+'''
 
-# Paper
-paper = ("""
-     _______
----'    ____)____
-           ______)
+paper = '''
+    _______
+---'   ____)____
+          ______)
           _______)
          _______)
 ---.__________)
-""")
+'''
 
-# Scissors
-scissors = ("""
+scissors = '''
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-""")
-choices = ["rock", "paper", "scissors"]
+'''
+choice = ["rock", "paper", "scissors"]
 ascii_art = [rock, paper, scissors]
 
-#Get user input
-user_input = int(input("Choose a number; 0 for rock, 1 for paper, or 2 for scissors: "))
+user_input = int(input("Please type 0 for rock, 1 for paper, 2 for scissors:"))
+user_choice = choice[user_input]
+print(f"You choose {user_choice}")
+print(ascii_art[user_input])
 
-#validate user input
-if user_input < 0 or user_input > 2:
-    print("Please choose a correct number from 0 -2")
-else:
-    user_index = choices[user_input]
-    user_choice = user_index
-    print(f"You choose {user_choice}")
-    print(ascii_art[user_input])
-
-
-
-#Computer randomly chooses a number
-computer_random_num = random.randint(0, 2)
-computer_choice = computer_random_num
-
+computer_input = random.randint(0, 2)
+computer_choice = choice[computer_input]
 print(f"Computer chooses {computer_choice}")
-print(ascii_art[computer_random_num])
+print(ascii_art[computer_input])
 
-if user_choice == computer_choice:
-    print("You Loose; GAMEOVER!!")
-else: 
-    print("You Won!!")
+if user_input == 0 and computer_input == 2:
+    print("You Win!!")
 
-
-
-
+# elif user_input == 2 and computer_input == 0:
+#     print("You Win!!")
+# elif user_input == 1 and computer_input == 0:
+##commented code above is correct, this is just to simplify it more
+elif user_input > computer_input:
+    print("You Win!!")
+elif user_input == computer_input:
+    print("It's a Draw!!")
+else:
+    print("You loose!!")
