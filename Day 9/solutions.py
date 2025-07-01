a53986677efb3a93bc598f0a95f2210ee9fa8a4a
GABRIEL OKOM <34310658+ougabriel@users.noreply.github.com>
2025-07-01 23:16:14 +0100

    Create solutions.py

---
+++ Day 9/solutions.py
@@ -0,0 +1,26 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades = {
+}
+
+for student in student_scores:
+    scores = student_scores[student]
+
+    if scores > 90:
+        grades = "Outstanding"
+    elif scores > 80:
+        grades = "Exceeds Expectation"
+    elif scores > 70:
+        grades = "Acceptable"
+    else:
+        grades = "Fail"
+
+student_grades[student] = grades
+
+print(student_grades)
