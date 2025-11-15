591d7bc4a1d10f40ab274998790fdb3d8235259f
Gabriel Okom <ougabriel@gmail.com>
2025-11-15 22:36:49 +0000

    Added scripts to number guess checker

--- Day 12/number_guessing_game.py
+++ Day 12/number_guessing_game.py
@@ -1,1 +1,16 @@
-import logo.py
+from art import logo
+
+print("Welcome to the Number Guessing Game: ")
+print("I'm thinking of a number between 1 and 100: ")
+
+
+difficulty = input(str("Please pick a difficulty level; type 'Easy' or 'Hard': ")).lower()
+if difficulty == "easy":
+        attempts = 5
+else:
+        attempts = 10
+print(f"You have {attempts} attempts remaining to guess the number.")
+
+
+
+
