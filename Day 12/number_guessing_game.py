2b648e01f6ce2069346216401f7457a2514266f3
Gabriel Okom <ougabriel@gmail.com>
2025-11-15 23:32:24 +0000

    Added loop to number_guess

--- Day 12/number_guessing_game.py
+++ Day 12/number_guessing_game.py
@@ -1,8 +1,8 @@
+import random
 from art import logo
 
 print("Welcome to the Number Guessing Game: ")
 print("I'm thinking of a number between 1 and 100: ")
-
 
 difficulty = input(str("Please pick a difficulty level; type 'Easy' or 'Hard': ")).lower()
 if difficulty == "easy":
@@ -10,6 +10,25 @@ if difficulty == "easy":
 else:
         attempts = 10
 print(f"You have {attempts} attempts remaining to guess the number.")
+
+choice = random.randint(1, 100)
+
+while True:
+    guess = int(input("Make a guess: "))
+
+    if guess > choice:
+        attempts -= 1
+        print("Too High.")
+    elif guess < choice:
+        attempts -= 1
+        print("Too Low.")
+    else:
+        print(f"You're right this time, you guessed {choice} correctly.")
+        
+        break
+
+
+
 
 
 
